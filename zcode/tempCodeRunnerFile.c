ac97e75274efb2cd0ebfb3896822dac3e39b4e4c
zunaid <zunaidhossain39@gmail.com>
2023-04-01 16:19:36 +0600

    zh.c

--- zcode/tempCodeRunnerFile.c
+++ zcode/tempCodeRunnerFile.c
@@ -1,1 +1,6 @@
- n=n/10;
+#include<stdio.h>
+int main(){
+    for(int i=0;i<=1;){
+        printf("z");
+    }
+}
--- zcode/zh.c
+++ zcode/zh.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
 int a[30][30],b[30][30],c[30][30];
-   int r1,c1,r2,c2;
+   int r1,c1,r2,c2,sum=0;
    printf("enter row and col of a matrix a");
    scanf("%d%d",&r1,&c1);
    printf("enter row and col of a matrix b");
@@ -11,21 +11,50 @@ int a[30][30],b[30][30],c[30][30];
     for(int i=0;i<r1;i++){
         printf("\n");
         for(int j=0;j<c1;j++){
-            printf("a[%d][%d]",i+1,j+1);
-            scanf("%d",&a[r1][c1]);
+            printf("a[%d][%d]=",i+1,j+1);
+            scanf("%d",&a[i][j]);
         }  printf("\n");
-printf("enter b matrix");
-    } for(int i=0;i<r2;i++){
+
+    } printf("enter b matrix");
+    for(int i=0;i<r2;i++){
          printf("\n");
         for(int j=0;j<c2;j++){
-            printf("b[%d][%d]",i+1,j+1);
-            scanf("%d",&b[r2][c2]);
+            printf("b[%d][%d]=",i+1,j+1);
+            scanf("%d",&b[i][j]);
         }
-    } printf("a=");
+    } printf("A=");
    for(int i=0;i<r1;i++){
     printf("\t");
+       
         for(int j=0;j<c1;j++){
-            printf("%d",a[i][j]);
-       }}}
+            printf("%d ",a[i][j]);
+            
+       }printf("\n");
+       }
+       printf("B=");
+   for(int i=0;i<r2;i++){
+    printf("\t");
+       
+        for(int j=0;j<c2;j++){
+            printf("%d ",b[i][j]);
+            
+       }printf("\n");
+       }
+        for(int i=0;i<r1;i++){
+        for(int j=0;j<c2;j++){
+            for(int k=0;k<c1;k++){
+                sum=sum+a[i][k]*b[k][j];
+            }c[i][j]=sum;
+             sum=0;}}
+         printf("A*B=");    
+    for(int i=0;i<r1;i++){
+    printf("\t");
+       
+        for(int j=0;j<c2;j++){
+            printf("%d ",c[i][j]);
+            
+       }printf("\n");
+       }    
+   }
        else{printf(" error!! \n can't perform");}
 }
